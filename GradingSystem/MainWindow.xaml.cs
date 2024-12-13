759661c118860acbbb87ad79b023e78ea26611bf
SoraSan01 <youremail@example.com>
2024-12-13 23:02:11 +0800

    Add project files.

---
+++ GradingSystem/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using GradingSystem.View;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GradingSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = new Dashboard();
+        }
+
+        private void dashboardBtn(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new Dashboard();
+        }
+
+        private void logoutBtn(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure you want to exit?", "Close", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+
+    }
+}
